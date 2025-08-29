2ca66810e5de7bafa612b7888bcb84a037bc8972
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-08-29 14:20:03 +0300

    Update GlobalUsings.cs

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -3,4 +3,4 @@ global using CleanArchitecture.Domain.Entities;
 global using CleanArchitecture.Domain.Enums;
 global using CleanArchitecture.Domain.Events;
 global using CleanArchitecture.Domain.Exceptions;
-global using CleanArchitecture.Domain.ValueObjects;
+global using CleanArchitecture.Domain.ValueObjects;  
